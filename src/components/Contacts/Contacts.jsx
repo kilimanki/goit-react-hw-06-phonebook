f7d5a4166d4faafcf70adf5d3c75af5d6187612a
kilimanki <www.slipperstrong@gmail.com>
2023-03-06 17:17:11 +0100

    zxc

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -5,6 +5,8 @@ import { List } from './List/List';
 import { Input } from './Input/Input';
 import { addContact, deleteContact } from 'redux/contacts/contacts-slice';
 import { setFilter } from 'redux/filter/filter-slice';
+import { getContacts } from 'redux/contacts/contacts-selector';
+import { getFilter } from 'redux/filter/filter-selectors';
 
 import css from './Contacts.module.css';
 const Contacts = () => {
@@ -12,8 +14,8 @@ const Contacts = () => {
   const handleChange = e => {
     dispatch(setFilter(e.target.value));
   };
-  const contacts = useSelector(store => store.contacts);
-  const filter = useSelector(store => store.filter);
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
 
   const addContacts = ({ name, number }) => {
     if (alreadyAdded({ name })) {
@@ -29,7 +31,6 @@ const Contacts = () => {
     return dublicate;
   };
   const deleteContacts = id => {
-    console.log(id);
     dispatch(deleteContact(id));
   };
 
@@ -56,85 +57,4 @@ const Contacts = () => {
     </>
   );
 };
-// class Contacts extends Component {
-//   state = {
-//     contacts: [],
-//     filter: '',
-//   };
-//   componentDidUpdate(_, prevState) {
-//     const { contacts } = this.state;
-//     localStorage.setItem('contacts', JSON.stringify(contacts));
-//   }
-//   componentDidMount() {
-//     const contacts = JSON.parse(localStorage.getItem('contacts'));
-//     if (contacts?.length) {
-//       this.setState({ contacts: contacts });
-//     }
-//   }
-// handleChange = e => {
-//   const { name, value } = e.target;
-//   this.setState({
-//     [name]: value,
-//   });
-// };
-
-// addContacts = ({ name, number }) => {
-//   if (this.alreadyAdded({ name })) {
-//     alert(`${name} is already added`);
-//     return;
-//   }
-//   this.setState(prevState => {
-//     const { contacts } = prevState;
-//     const newContact = {
-//       name,
-//       number,
-//       id: nanoid(),
-//     };
-//     return { contacts: [...contacts, newContact] };
-//   });
-// };
-// alreadyAdded = ({ name }) => {
-//   const { contacts } = this.state;
-
-//   const dublicate = contacts.find(item => {
-//     return item.name.toLowerCase() === name.toLowerCase();
-//   });
-//   return dublicate;
-// };
-
-// deleteContacts = id => {
-//   this.setState(prevState => {
-//     const newItems = prevState.contacts.filter(item => item.id !== id);
-//     return {
-//       contacts: newItems,
-//     };
-//   });
-// };
-
-// filteredContacts() {
-//   const { filter, contacts } = this.state;
-//   if (!filter) {
-//     return contacts;
-//   }
-
-//   const result = contacts.filter(item => {
-//     return item.name.toLowerCase().includes(filter.toLowerCase());
-//   });
-//   return result;
-// }
-//   render() {
-// const items = this.filteredContacts();
-// const { filter } = this.state;
-// return (
-//   <>
-//     <Form onSubmit={this.addContacts} />
-//     <h2 className={css.title}>Contacts</h2>
-//     <div className={css.block}>
-//       <Input handleChange={this.handleChange} filter={filter} />
-//       <List items={items} onclick={this.deleteContacts} />
-//     </div>
-//   </>
-// );
-//   }
-// }
 export default Contacts;
--- src/redux/contacts/contacts-selector.js
+++ src/redux/contacts/contacts-selector.js
@@ -1,1 +1,1 @@
-export const getContacts = store => store.contacts;
+export const getContacts = state => state.contacts;
